46cfaa830d13137fec0fd57488d95398657db3ec
gekumeeee <45541607+gekumeeee@users.noreply.github.com>
2018-12-02 23:53:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('ready', () => { console.log(Logged in as ${client.user.tag} !); 
-client.user.setActivity("الكلام الي بعد الواتشنق",{type: 'WATCHING'}); }); 
 
 
 
