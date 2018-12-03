9e6eaf147b3dd01f96fa7fccbb977249731a1562
gekumeeee <45541607+gekumeeee@users.noreply.github.com>
2018-12-03 19:18:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -227,3 +227,23 @@ client.on('message', message => {
     }
 });
 
+client.on('message', message => {
+    var argresult = message.content.split(`).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith('$' + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(white_check_mark   ${argresult})
+  } else 
+  if (message.content.startsWith('$' + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(white_check_mark   ${argresult})
+  } else 
+  if (message.content.startsWith('$' + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(white_check_mark   ${argresult}) ////Mal_Team
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/ℊℯℯ𝖪𝒴 server!!help");
+      message.channel.sendMessage(white_check_mark   ${argresult}`)////Mal_Team
+  }
