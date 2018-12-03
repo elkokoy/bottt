f28c7c4657fa90a92f1c7719ff81489f5ef91540
gekumeeee <45541607+gekumeeee@users.noreply.github.com>
2018-12-03 06:34:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -82,7 +82,7 @@ client.on('message', message => {
     var bc = new  
        Discord.RichEmbed()
        .setColor('RANDOM')
-       .setTitle('البرودكاست') .addField('السيرفر', message.guild.name) .addField('المرسل', message.author.username)  
+       .setTitle('Welcome guys') .addField('السيرفر', message.guild.name) .addField('المرسل', message.author.username)  
        .addField('الرساله', args)  
        .setThumbnail(message.author.avatarURL)  
        .setFooter(copy, client.user.avatarURL); 
