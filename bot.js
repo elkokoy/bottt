75eeb51e7bd080d6e408d1f912587008165e302a
gekumeeee <45541607+gekumeeee@users.noreply.github.com>
2018-12-03 19:29:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -226,3 +226,37 @@ client.on('message', message => {
   message.channel.sendEmbed(embed);
     }
 });
+adminprefix = 'y!'
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send("**:white_check_mark: | The Playing Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'wat')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send("**:white_check_mark: | The Watching Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'lis')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send("**:white_check_mark: | The Listening Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/i_kahrba999");
+      message.channel.send("**:white_check_mark: | The Streaming Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
