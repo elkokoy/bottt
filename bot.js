f9e7064b90abfafacf9e43f6f79e4686feea36fa
gekumeeee <45541607+gekumeeee@users.noreply.github.com>
2018-12-05 14:11:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -337,41 +337,20 @@ client.on('message', message => {
        })
      }
      });
-client.on('message', message => {
-       if (message.author.x5bz) return;
-       if (!message.content.startsWith(prefix)) return;
+client.on('message',async msg => {
+  if(msg.content.startsWith(prefix + "user")) {
+     time.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+  setInterval(() => {
+      var currentTime = new Date(),
+Year = currentTime.getFullYear(),
+Month = currentTime.getMonth() + 1,
+Dat = currentTime.getDate()
+  },1000);
+  }
 
-       let command = message.content.split(" ")[0];
-       command = command.slice(prefix.length);
+});
 
-       let args = message.content.split(" ").slice(1);
-
-       if (command == "ban") {
-                    if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-       if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
-       if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
-       let user = message.mentions.users.first();
-       let reason = message.content.split(" ").slice(2).join(" ");
-       /*let b5bzlog = client.channels.find("name", "5bz-log");
-       if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
-       if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-       if(!reason) return message.reply ("**اكتب سبب البان**");
-       if (!message.guild.member(user)
-       .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه اعلي**");
-
-       message.guild.member(user).ban(7, user);
-
-       const banembed = new Discord.RichEmbed()
-       .setAuthor(`BANNED!`, user.displayAvatarURL)
-       .setColor("RANDOM")
-       .setTimestamp()
-       .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
-       .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-       .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
-       message.channel.send({
-         embed : banembed
-       })
-     }
-     });
 client.login(process.env.BOT_TOKEN);
